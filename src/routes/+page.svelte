a6d6ccad93ff79dfda760e22deca421a523d0762
levtus <i@alexander.systems>
2024-03-03 11:00:08 -0600

    Add boilerplate, styles and light/dark mode logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { MapLibre, NavigationControl, Marker,  Popup, FullscreenControl, ScaleControl, Control, ControlGroup, ControlButton } from 'svelte-maplibre';
+    import DarkMode from "svelte-dark-mode";
+    import { onMount } from 'svelte';
+	import type { Theme } from 'svelte-dark-mode/types/DarkMode.svelte';
+
+    let theme: Theme | undefined;
+
+    $: switchTheme = (theme === "dark" ? "light" : "dark") as Theme;
+</script>
+
+<svelte:head>
+    <title>Strava Multi-Activity Mapper</title> 
+    <meta name="description" content="Advanced & modern utility to map all of your Strava activities onto a customizable and ">
+    <meta name="keywords" content="Strava, Mapper, Ride">
+    <meta name="author" content="Alexander Akira Weimer">
+</svelte:head>
