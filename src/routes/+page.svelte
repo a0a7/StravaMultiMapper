dffca4ef3f646049a546eb8ffb4c7e7238846a22
levtus <i@alexander.systems>
2024-03-05 08:01:50 -0600

    Component-ize map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,9 @@
     import Fa from 'svelte-fa'
     import { faMoon, faSun} from '@fortawesome/free-solid-svg-icons'
     import { faGithub } from '@fortawesome/free-brands-svg-icons';
-    import { MapLibre, NavigationControl, Marker,  Popup, FullscreenControl, ScaleControl, Control, ControlGroup, ControlButton } from 'svelte-maplibre';
     import * as Table from "$lib/components/ui/table";
     import { Button } from "$lib/components/ui/button";
-    import { toggleMode } from "mode-watcher";    
+    import { toggleMode, mode } from "mode-watcher";    
 </script>
 
 <svelte:head>
@@ -13,10 +12,12 @@
     <meta name="description" content="Advanced & modern utility to map all of your Strava activities onto a customizable and ">
     <meta name="keywords" content="Strava, Mapper, Ride">
     <meta name="author" content="Alexander Akira Weimer">
+    <script src='https://unpkg.com/maplibre-gl@latest/dist/maplibre-gl.js'></script>
+    <link href='https://unpkg.com/maplibre-gl@latest/dist/maplibre-gl.css' rel='stylesheet' />    
 </svelte:head>
 
-<div class="sm:flex sm:flex-col md:flex-row">
-    <div class="w-full md:w-[25vw] md:h-[100vh]"> 
+<div class="sm:flex sm:flex-col md:flex-row h-max">
+    <div class="w-full md:w-[25vw] md:h-[100vh] absolute md:static"> 
         <Button on:click={toggleMode} variant="outline" size="icon">
             <img src="icons/sun.svg" alt="Switch to light mode"
               class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
@@ -27,5 +28,12 @@
             <span class="sr-only">Toggle theme</span>
           </Button>
     </div>
-    <div class="w-full md:w-[75vw] md:h-[100vh]">Div 2</div>
+    <div class="w-full md:w-[75vw] h-[40vh] md:h-[100vh]">
+    </div>
 </div>
+
+<style>
+  :global(.map) {
+    height: inherit;
+  }
+</style>
