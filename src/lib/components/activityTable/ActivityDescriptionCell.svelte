8a805be740a4fa60e6128ab8ae4aec2195e19e4f
levtus <i@alexander.systems>
2024-03-17 17:25:11 -0500

    Add activity tooltip on hover to icon

--- src/lib/components/activityTable/ActivityDescriptionCell.svelte
+++ src/lib/components/activityTable/ActivityDescriptionCell.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { getActivityIconURL, getCommuteIconURL } from '$lib/utils'
+    import * as Tooltip from "$lib/components/ui/tooltip/index.js";
 	import '$lib/components/sidebar/svg-styles.css';
 
     export let name: string;
@@ -9,9 +10,16 @@
 </script>
 <div class="flex">
     <p class="inline-block truncate">{name}</p>
-    <div class="inline-block px-1 w-4 h-4 translate-y-1">
-        {@html getActivityIconURL(sport_type)}
-    </div>
+    <Tooltip.Root openDelay={50}>
+        <Tooltip.Trigger >
+            <div class="inline-block mx-1 w-4 h-4 translate-y-1">
+                {@html getActivityIconURL(sport_type)}
+            </div>
+        </Tooltip.Trigger>
+        <Tooltip.Content class="bg-card">
+          <p>{sport_type.replace(/(?<!^)([A-Z])/g, ' $1')}</p>
+        </Tooltip.Content>
+    </Tooltip.Root>
     <!-- Commute Icon
     <div class="svg-icon inline-block w-3 h-3 left-4 translate-y-[6px]">
         {@html getCommuteIconURL(commute)}
