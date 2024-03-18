a55310efdceadd0b4776a97e5bd442c551625693
levtus <i@alexander.systems>
2024-03-18 12:39:19 -0500

    Lint

--- src/lib/components/sidebar/MapConfigPanel.svelte
+++ src/lib/components/sidebar/MapConfigPanel.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-    import * as Card from '$lib/components/ui/card';
-    import * as Select from "$lib/components/ui/select"
-    import * as Dropdown from "$lib/components/ui/dropdown-menu"
+	import * as Card from '$lib/components/ui/card';
+	import * as Select from '$lib/components/ui/select';
+	import * as Dropdown from '$lib/components/ui/dropdown-menu';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { Separator } from '$lib/components/ui/separator';
-    import { Label } from "$lib/components/ui/label"
-    import BasemapSelect from '$lib/components/sidebar/map-config-components/BasemapSelect.svelte';
+	import { Label } from '$lib/components/ui/label';
+	import BasemapSelect from '$lib/components/sidebar/map-config-components/BasemapSelect.svelte';
 </script>
 
 <Card.Root>
-    <Card.Content>
-        <BasemapSelect />
-    </Card.Content>
+	<Card.Content>
+		<BasemapSelect />
+	</Card.Content>
 </Card.Root>
--- src/lib/components/sidebar/map-config-components/BasemapSelect.svelte
+++ src/lib/components/sidebar/map-config-components/BasemapSelect.svelte
@@ -1,94 +1,95 @@
 <script lang="ts">
-    import Check from "svelte-radix/Check.svelte";
-    import CaretSort from "svelte-radix/CaretSort.svelte";
-    import * as Command from "$lib/components/ui/command/index.js";
-    import * as Popover from "$lib/components/ui/popover/index.js";
-    import { Label } from "$lib/components/ui/label/";
-    import { Button } from "$lib/components/ui/button/index.js";
-    import { cn } from "$lib/utils.js";
-    import { tick } from "svelte";
-   
-    const basemaps = [
-      {
-        value: "proton",
-        label: "Light"
-      },
-      {
-        value: "darkmatter",
-        label: "Dark"
-      },
-      {
-        value: "summer",
-        label: "Summer"
-      },
-      {
-        value: "winter",
-        label: "Winter"
-      },
-      {
-        value: "streets",
-        label: "Streets"
-      },
-      {
-        value: "satellite",
-        label: "Satellite"
-      }
-    ];
-  
-    let open = false;
-    let value = "";
-   
-    $: selectedValue = basemaps.find((f) => f.value === value)?.label ?? "Select a basemap style...";
-   
-    function closeAndFocusTrigger(triggerId: string) {
-      open = false;
-      tick().then(() => {
-        document.getElementById(triggerId)?.focus();
-      });
-    }
+	import Check from 'svelte-radix/Check.svelte';
+	import CaretSort from 'svelte-radix/CaretSort.svelte';
+	import * as Command from '$lib/components/ui/command/index.js';
+	import * as Popover from '$lib/components/ui/popover/index.js';
+	import { Label } from '$lib/components/ui/label/';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { cn } from '$lib/utils.js';
+	import { tick } from 'svelte';
+
+	const basemaps = [
+		{
+			value: 'proton',
+			label: 'Light'
+		},
+		{
+			value: 'darkmatter',
+			label: 'Dark'
+		},
+		{
+			value: 'summer',
+			label: 'Summer'
+		},
+		{
+			value: 'winter',
+			label: 'Winter'
+		},
+		{
+			value: 'streets',
+			label: 'Streets'
+		},
+		{
+			value: 'satellite',
+			label: 'Satellite'
+		}
+	];
+
+	let open = false;
+	let value = '';
+
+	$: selectedValue = basemaps.find((f) => f.value === value)?.label ?? 'Select a basemap style...';
+
+	function closeAndFocusTrigger(triggerId: string) {
+		open = false;
+		tick().then(() => {
+			document.getElementById(triggerId)?.focus();
+		});
+	}
 </script>
+
 <Label for="basemap-select">Basemap</Label>
 <Popover.Root bind:open let:ids>
-    <Popover.Trigger asChild id="basemap-select" let:builder>
-        <Button
-        builders={[builder]}
-        variant="outline"
-        role="combobox"
-        aria-expanded={open}
-        class="w-[200px] justify-between"
-        >
-        {selectedValue}
-        <CaretSort class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-    </Popover.Trigger>
-    <Popover.Content class="w-[200px] p-0">
-        <Command.Root>
-        <Command.Group heading="Palettes">
-          {#each basemaps.slice(0, 4) as basemap}
-            <Command.Item
-              value={basemap.value}
-              onSelect={(currentValue) => {
-                value = currentValue;
-                closeAndFocusTrigger(ids.trigger);
-              }}
-            >
-              {basemap.label}
-            </Command.Item>
-          {/each}
-          </Command.Group>
-          <Command.Group heading="Data Sources">
-            {#each basemaps.slice(4, 6) as basemap}
-            <Command.Item
-                  value={basemap.value}
-                  onSelect={(currentValue) => {
-                  value = currentValue;
-                  closeAndFocusTrigger(ids.trigger);
-                  }}
-              >
-              {basemap.label}
-              </Command.Item>
-              {/each}
-        </Command.Group>
-        </Command.Root>
-    </Popover.Content>
+	<Popover.Trigger asChild id="basemap-select" let:builder>
+		<Button
+			builders={[builder]}
+			variant="outline"
+			role="combobox"
+			aria-expanded={open}
+			class="w-[200px] justify-between"
+		>
+			{selectedValue}
+			<CaretSort class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+		</Button>
+	</Popover.Trigger>
+	<Popover.Content class="w-[200px] p-0">
+		<Command.Root>
+			<Command.Group heading="Palettes">
+				{#each basemaps.slice(0, 4) as basemap}
+					<Command.Item
+						value={basemap.value}
+						onSelect={(currentValue) => {
+							value = currentValue;
+							closeAndFocusTrigger(ids.trigger);
+						}}
+					>
+						{basemap.label}
+					</Command.Item>
+				{/each}
+			</Command.Group>
+			<Command.Group heading="Data Sources">
+				{#each basemaps.slice(4, 6) as basemap}
+					<Command.Item
+						value={basemap.value}
+						onSelect={(currentValue) => {
+							value = currentValue;
+							closeAndFocusTrigger(ids.trigger);
+						}}
+					>
+						{basemap.label}
+					</Command.Item>
+				{/each}
+			</Command.Group>
+		</Command.Root>
+	</Popover.Content>
 </Popover.Root>
