04f85256c69e2be0da6c6ead5f0409d48988b99a
levtus <i@alexander.systems>
2024-03-19 08:46:07 -0500

    Improve loading screen mobile appearance

--- src/lib/components/Loading.svelte
+++ src/lib/components/Loading.svelte
@@ -9,15 +9,13 @@
 	<WordmarkLogo />
 	<div class="mt-[75px] flex flex-col items-center text-muted-foreground">
 		<SyncLoader size="60" color="#313c47" unit="px" duration="1s" />
-		<h2 class="py-4">The page is now loading. This shouldn't take long.</h2>
-		<h3>
-			Experiencing issues? Report them on <a
+		<h2 class="py-4 mx-4">The&nbsp;page&nbsp;is&nbsp;now&nbsp;loading. This&nbsp;shouldn't&nbsp;take&nbsp;long.</h2>
+		<h3 >
+			Experiencing&nbsp;issues?</h3><h3>Report&nbsp;them&nbsp;on&nbsp;<a
 				href="https://github.com/sudolev/StravaMultiMapper"
 				target="_blank"
 				rel="noopener noreferrer">GitHub</a
-			>
-			or
-			<a href="https://discord.gg/5P3AYFrwQG" target="_blank" rel="noopener noreferrer">Discord</a>.
+			>&nbsp;or&nbsp;<a href="https://discord.gg/5P3AYFrwQG" target="_blank" rel="noopener noreferrer">Discord</a>.
 		</h3>
 	</div>
 </div>
