ad1a45b233a402225b441e8891fa56ab8d898b35
levtus <i@alexander.systems>
2024-03-20 10:18:57 -0500

    Reorganize settings panel

--- src/lib/components/sidebar/MapConfigPanel.svelte
+++ src/lib/components/sidebar/MapConfigPanel.svelte
@@ -6,36 +6,17 @@
 	import { Separator } from '$lib/components/ui/separator';
 	import { Label } from '$lib/components/ui/label';
 	import BasemapSelect from '$lib/components/sidebar/map-config-components/BasemapSelect.svelte';
+	import CommuteSelect from '$lib/components/sidebar/map-config-components/CommuteSelect.svelte';
 	import ActivityTypeSelect from '$lib/components/sidebar/map-config-components/ActivityTypeSelect.svelte';
 	import DateRangePicker from '$lib/components/sidebar/map-config-components/DateRangePicker.svelte';
 	import type { StravaActivity } from '$lib/activities';
-
 	export let activities: StravaActivity[];
 </script>
 
-<div class="w-full px-6">
-	<table>
-		<tr>
-			<td>Map appearance:</td>
-			<td><BasemapSelect /></td>
-		</tr>
-		<tr>
-			<td>Date range:</td>
-			<td><DateRangePicker {activities} /></td>
-		</tr>
-		<tr>
-			<td>Activity type:</td>
-			<td><ActivityTypeSelect /></td>
-		</tr>
-		<tr>
-			<td>Commute status:</td>
-			<td></td>
-		</tr>
-	</table>
+<div class="w-full px-6 flex flex-col gap-2">
+	<DateRangePicker {activities} />
+	<ActivityTypeSelect />
+	<CommuteSelect />
+	<Separator class="mb-3 mx-5 w-[calc(100vw-2.5rem)] md:w-auto" />
+	<BasemapSelect />
 </div>
-
-<style>
-	td {
-		@apply pr-4 pb-2 leading-none;
-	}
-</style>
