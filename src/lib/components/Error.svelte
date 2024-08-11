f5ddc69bac30b5e6a1559364e523e0fece452af8
lev <alexander.weimer@proton.me>
2024-08-11 14:12:22 -0500

    More detailed error popup.

--- src/lib/components/Error.svelte
+++ src/lib/components/Error.svelte
@@ -7,7 +7,7 @@
 	<AlertDialog.Content>
 		<AlertDialog.Header>
 			<AlertDialog.Title>An error has occured!</AlertDialog.Title>
-			<AlertDialog.Description>{content}</AlertDialog.Description>
+			<AlertDialog.Description>{content}<br><br><p class="font-bold">Reload the page and try again. If the issue persists, please feel free to report it in my <a href="https://discord.gg/kw7hx73wVA">Discord Server</a> and I'll try my best to fix it.</p>			</AlertDialog.Description>
 		</AlertDialog.Header>
 		<AlertDialog.Footer>
 			<AlertDialog.Cancel>Close</AlertDialog.Cancel>
